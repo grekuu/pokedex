8fc64be37a94908bfea40b7417758928f4c4702a
grekuu <gracjanprusik@gmail.com>
2023-02-12 11:49:23 +0100

    read value from input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { getAllPokemons } from "../../redux/pokemonsSlice";
 import "./header.scss";
 
 const Header = () => {
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log(filter);
+  }, [filter]);
+
   return (
     <div className="header">
       <h2>Pokedex</h2>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setFilter(e.target.value)}
+      />
     </div>
   );
 };
